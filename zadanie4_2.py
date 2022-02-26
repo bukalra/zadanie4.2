b92e5332a27d832713ffe06354fd3bb3ad0594f1
Radek Bukalski <bukalskiradek@gmail.com>
2022-02-26 23:00:52 +0100

    initial commit

---
+++ zadanie4_2.py
@@ -0,0 +1,15 @@
+#zadanie 4.2
+
+import re
+
+
+word = input('Enter a word:')
+
+def polindrom(word):
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+
+result = polindrom(word)
+print(result)
